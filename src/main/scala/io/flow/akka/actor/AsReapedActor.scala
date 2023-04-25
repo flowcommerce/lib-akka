8b0ceed53f124372e13a70dc076df9125fd41d5e
Jack Lynch <jack@flow.io>
2023-04-25 17:03:53 +0100

    Move newly added reaper actor from lib-play (#121)

---
+++ src/main/scala/io/flow/akka/actor/AsReapedActor.scala
@@ -0,0 +1,10 @@
+package io.flow.akka.actor
+
+import akka.actor.Actor
+
+/** Mixin to have ActorRef added to set of actors to be terminated early during CoordinatedShutdown. */
+trait AsReapedActor {
+  this: Actor =>
+
+  Reaper.get(context.system).watch(self)
+}
