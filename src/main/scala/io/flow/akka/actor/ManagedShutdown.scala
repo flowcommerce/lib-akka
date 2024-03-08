5822e1b43b7f892fd88631e50b66252cbcebf1e1
Jack Lynch <jack@flow.io>
2024-03-08 11:47:13 +0000

    FIN-2041 Add phases to CoordinatedShutdownModule

--- src/main/scala/io/flow/akka/actor/ManagedShutdown.scala
+++ src/main/scala/io/flow/akka/actor/ManagedShutdown.scala
@@ -1,16 +1,34 @@
 package io.flow.akka.actor
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, CoordinatedShutdown}
 import io.flow.util.{ShutdownNotified, Shutdownable}
+
+sealed trait ManagedShutdownPhase // Subset of service phases listed in CoordinatedShutdown
+
+object ManagedShutdownPhase {
+  case object ServiceUnbind extends ManagedShutdownPhase {
+    override def toString: String = CoordinatedShutdown.PhaseServiceUnbind
+  }
+  case object ServiceRequestsDone extends ManagedShutdownPhase {
+    override def toString: String = CoordinatedShutdown.PhaseServiceRequestsDone
+  }
+  case object ServiceStop extends ManagedShutdownPhase {
+    override def toString: String = CoordinatedShutdown.PhaseServiceStop
+  }
+
+  val All: Seq[ManagedShutdownPhase] = Seq(ServiceUnbind, ServiceRequestsDone, ServiceStop)
+}
 
 trait ManagedShutdown extends ShutdownNotified {
   self: Shutdownable =>
+
+  def shutdownPhase: ManagedShutdownPhase = ManagedShutdownPhase.ServiceRequestsDone
 
   @volatile private var shutdownState: Boolean = false
 
   def system: ActorSystem
 
-  Reaper.get(system).watch(this)
+  Reaper.get(system).watch(this, shutdownPhase)
 
   def shutdown(): Unit = {
     shutdownState = true
@@ -24,5 +42,4 @@ trait ManagedShutdown extends ShutdownNotified {
       shutdownState = true
     }
   }
-
 }
